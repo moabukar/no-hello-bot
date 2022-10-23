ecadf0acc661e08c3e540fb8ef756967ae2a0c8d
moabukar <mo-abukar10@hotmail.com>
2022-10-23 18:35:13 +0100

    add read config function

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
 import (
-	""
+	"fmt"
 )
+
+func main(){
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error)
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan, struct{})
+	return
+}
