846517e8c9245eeb98a72fb51b81b8a1ab7a13f5
moabukar <mo-abukar10@hotmail.com>
2023-06-11 17:18:47 +0100

    back to old method

--- config/config.go
+++ config/config.go
@@ -1,8 +1,9 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
 var (
@@ -21,33 +22,61 @@ func ReadConfig() error {
 
 	fmt.Println("Reading config ...")
 
-	// file, err := ioutil.ReadFile("./docs/config.json")
+	file, err := ioutil.ReadFile("./docs/config.json")
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return err
-	// }
-
-	// fmt.Println(string(file))
-
-	// err = json.Unmarshal(file, &config)
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return err
-	// }
-
-	// Token = config.Token
-	// BotPrefix = config.BotPrefix
-	// ---------------------------------
-	// Implement method of env vars //
-
-	Token = os.Getenv("TOKEN")
-	BotPrefix = "No-Hello-Bot"
-
-	if Token == "" || BotPrefix == "" {
-		return fmt.Errorf("BOT_TOKEN and BOT_PREFIX environment variables must be set")
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
 	}
+
+	fmt.Println(string(file))
+
+	err = json.Unmarshal(file, &config)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
 
 	return nil
 }
+
+// Environment Vars method below
+
+// func ReadConfig() error {
+
+// 	fmt.Println("Reading config ...")
+
+// 	// file, err := ioutil.ReadFile("./docs/config.json")
+
+// 	// if err != nil {
+// 	// 	fmt.Println(err.Error())
+// 	// 	return err
+// 	// }
+
+// 	// fmt.Println(string(file))
+
+// 	// err = json.Unmarshal(file, &config)
+
+// 	// if err != nil {
+// 	// 	fmt.Println(err.Error())
+// 	// 	return err
+// 	// }
+
+// 	// Token = config.Token
+// 	// BotPrefix = config.BotPrefix
+// 	// ---------------------------------
+// 	// Implement method of env vars //
+
+// 	Token = os.Getenv("TOKEN")
+// 	BotPrefix = "No-Hello-Bot"
+
+// 	if Token == "" || BotPrefix == "" {
+// 		return fmt.Errorf("BOT_TOKEN and BOT_PREFIX environment variables must be set")
+// 	}
+
+// 	return nil
+// }
