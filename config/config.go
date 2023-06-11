f3fe0f519f2bd64e70ae424e8d8bfeb7dc6b007b
moabukar <mo-abukar10@hotmail.com>
2023-06-11 17:56:46 +0100

    use env var method

--- config/config.go
+++ config/config.go
@@ -1,9 +1,8 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 var (
@@ -18,65 +17,65 @@ type configStruct struct {
 	BotPrefix string `json:"BotPrefix"`
 }
 
-func ReadConfig() error {
-
-	fmt.Println("Reading config ...")
-
-	file, err := ioutil.ReadFile("./docs/config.json")
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-
-	fmt.Println(string(file))
-
-	err = json.Unmarshal(file, &config)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return err
-	}
-
-	Token = config.Token
-	BotPrefix = config.BotPrefix
-
-	return nil
-}
-
-// Environment Vars method below
-
 // func ReadConfig() error {
 
 // 	fmt.Println("Reading config ...")
 
-// 	// file, err := ioutil.ReadFile("./docs/config.json")
+// 	file, err := ioutil.ReadFile("./docs/config.json")
 
-// 	// if err != nil {
-// 	// 	fmt.Println(err.Error())
-// 	// 	return err
-// 	// }
-
-// 	// fmt.Println(string(file))
-
-// 	// err = json.Unmarshal(file, &config)
-
-// 	// if err != nil {
-// 	// 	fmt.Println(err.Error())
-// 	// 	return err
-// 	// }
-
-// 	// Token = config.Token
-// 	// BotPrefix = config.BotPrefix
-// 	// ---------------------------------
-// 	// Implement method of env vars //
-
-// 	Token = os.Getenv("TOKEN")
-// 	BotPrefix = "No-Hello-Bot"
-
-// 	if Token == "" || BotPrefix == "" {
-// 		return fmt.Errorf("BOT_TOKEN and BOT_PREFIX environment variables must be set")
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return err
 // 	}
+
+// 	fmt.Println(string(file))
+
+// 	err = json.Unmarshal(file, &config)
+
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return err
+// 	}
+
+// 	Token = config.Token
+// 	BotPrefix = config.BotPrefix
 
 // 	return nil
 // }
+
+// Environment Vars method below
+
+func ReadConfig() error {
+
+	fmt.Println("Reading config ...")
+
+	// file, err := ioutil.ReadFile("./docs/config.json")
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return err
+	// }
+
+	// fmt.Println(string(file))
+
+	// err = json.Unmarshal(file, &config)
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return err
+	// }
+
+	// Token = config.Token
+	// BotPrefix = config.BotPrefix
+	// ---------------------------------
+	// Implement method of env vars //
+
+	Token = os.Getenv("TOKEN")
+	BotPrefix = "No-Hello-Bot"
+
+	if Token == "" || BotPrefix == "" {
+		return fmt.Errorf("BOT_TOKEN and BOT_PREFIX environment variables must be set")
+	}
+
+	return nil
+}
